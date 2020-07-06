8a386a44a60dff92feedea78a262217d8db97f0e
毛煜苏 <maoyusu@corp.netease.com>
2020-07-06 13:15:12 +0800

    feat: 实现标准的粉笔账号密码登录

--- src/service/loginService.js
+++ src/service/loginService.js
@@ -2,14 +2,25 @@ const request = require('request');
 const setCookie = require('set-cookie-parser');
 
 
+function queryString(n) {
+    var t = "";
+    for (let e in n)
+        t += e + "=" + encodeURIComponent(n[e]) + "&";
+    return t.slice(0, -1)
+}
+
 /**
  * 返回的是 Cookie
  */
 exports.login = async function (phone, password, captcha) {
-    let loginBody = `phone=${phone}&password=${password}`;
+    let loginBody = {
+        phone,
+        password
+    };
     if (captcha != null && captcha !== '') {
-        loginBody += `&captcha=${captcha}`;
+        loginBody.captcha = captcha;
     }
+
     return await new Promise(function (resolve, reject) {
         request({
             url: 'https://tiku.fenbi.com/api/users/loginV2',
@@ -18,7 +29,7 @@ exports.login = async function (phone, password, captcha) {
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
             },
-            body: loginBody,
+            body: queryString(loginBody),
         }, function (err, httpResponse, body) {
             if (err) reject(err);
             resolve(setCookie.parse(httpResponse.headers['set-cookie']));
